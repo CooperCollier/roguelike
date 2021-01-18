fb94146e30b67d4d1e2dda5fab106d0b7aca04d6
Cooper Collier <collier@berkeley.edu>
2021-01-17 17:46:34 -0800

    added gold chest

---
+++ Assets/Scripts/ChestGold.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestGold : Chest {
+
+    //--------------------------------------------------------------------------------
+
+    public override void UpdateAnimation() {
+
+        string animationName;
+
+        if (opened) {
+            animationName = "ChestGoldOpen";
+        } else {
+            animationName = "ChestGoldClosed";
+        }
+
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName(animationName)) {
+            animator.Play(animationName, 0);
+        }
+
+    }
+
+    //--------------------------------------------------------------------------------
+
+    public override int GenerateCoins() {
+    	return Random.Range(30, 40);
+    }
+
+    public override int GenerateApples() {
+    	return 0;
+    }
+
+    public override bool GenerateCoffee() {
+    	return false;
+    }
+
+    public override bool GenerateGoldApple() {
+    	return true;
+    }
+
+    public override bool GenerateBook() {
+    	return false;
+    }
+
+    public override Spell GenerateScroll() {
+    	return null;
+    }
+
+    //--------------------------------------------------------------------------------
+
+}
